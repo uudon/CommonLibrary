fd44daa30c415f70987ace1c6b0c83ac330f9f83
uudon <uudon@163.com>
2022-11-07 16:46:29 +0800

    更新api

--- app/src/main/java/com/don/base/library/MainActivity.kt
+++ app/src/main/java/com/don/base/library/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        Log.setLogSwitch(false)
+        Log.setLogSwitch(true)
         Log.d("我是笨蛋")
     }
 }
